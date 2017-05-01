05a17cc20d5124fa807e32239e7aceb50102a57b
Vincent Predoehl <vpredoehl@gmail.com>
2017-04-30 22:55:26 -0500

    choose rollover from photo library

---
+++ Manifestation/RolloverViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Manifestation
+//
+//  Created by Vincent Predoehl on 4/30/17.
+//  Copyright © 2017 Vincent Predoehl. All rights reserved.
+//
+
+import UIKit
+
+class RolloverViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+    @IBOutlet weak var chiImageView: UIImageView!
+    @IBOutlet weak var rolloverImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func playRollover(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func takePicture(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let img = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        rolloverImageView.image = img
+        dismiss(animated: true, completion: nil)
+    }
+}
+
