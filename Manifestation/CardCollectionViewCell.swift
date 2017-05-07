ac6ee3ba3f09bf789e0487f68effcf0bd673d354
Vincent Predoehl <vpredoehl@gmail.com>
2017-05-07 12:14:12 -0500

    rename AoD images

---
+++ Manifestation/CardCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CardCollectionViewCell.swift
+//  Manifestation
+//
+//  Created by Vincent Predoehl on 5/7/17.
+//  Copyright © 2017 Vincent Predoehl. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
---
+++ Manifestation/CardsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CardsViewController.swift
+//  Manifestation
+//
+//  Created by Vincent Predoehl on 5/7/17.
+//  Copyright © 2017 Vincent Predoehl. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UICollectionViewController {
+    
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+    
+//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        
+//    }
+}
